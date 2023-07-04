fec932ca6f16df1eb32b8ac19fc181058dec3a84
Zachary Turner <zakkturner@yahoo.com>
2023-07-04 16:33:23 -0400

    fixing press imgs

--- components/about/Press.vue
+++ components/about/Press.vue
@@ -1,110 +1,123 @@
-<template >
-    <div class="row">
-        <div class="col-sm-12 col-md-12 subpage-block">
-          <div class="block-title">
-            <h3>Press</h3>
-          </div>
-          <div class="testimonials owl-carousel">
-            <!-- Testimonial 1 -->
-            <div class="testimonial-item">
-              <!-- Testimonial Content -->
-              <div class="testimonial-content">
-                <div class="testimonial-text">
-                    <img src="images/press/cnn-press.png" alt="CNN Delta Starbucks">
-                </div>
-            </div>            
-            <!-- /Testimonial Content -->  
-            <!-- Testimonial Author -->
-            <div class="testimonial-credits">
-                <!-- Picture -->
-                <div class="testimonial-picture">
-                    <img src="images/press/cnn-logo.png" alt="CNN Logo">
-                </div>              
-                <!-- /Picture -->
-                <!-- Testimonial author information -->
-                <div class="testimonial-author-info">
-                    <p class="testimonial-author"><a href="https://www.cnn.com/2022/10/12/business/starbucks-rewards-delta-skymiles-partnership/index.html">CNN </a></p>
-                    <p class="testimonial-firm"> Delta X Starbucks Skymiles Program Website</p>
-                </div>
+<template>
+  <div class="row">
+    <div class="col-sm-12 col-md-12 subpage-block">
+      <div class="block-title">
+        <h3>Press</h3>
+      </div>
+      <div class="testimonials owl-carousel">
+        <!-- Testimonial 1 -->
+        <div class="testimonial-item">
+          <!-- Testimonial Content -->
+          <div class="testimonial-content">
+            <div class="testimonial-text">
+              <img
+                src="/images/press/cnn-press.png"
+                alt="CNN Delta Starbucks"
+              />
             </div>
-            <!-- /Testimonial author information -->               
+          </div>
+          <!-- /Testimonial Content -->
+          <!-- Testimonial Author -->
+          <div class="testimonial-credits">
+            <!-- Picture -->
+            <div class="testimonial-picture">
+              <img src="/images/press/cnn-logo.png" alt="CNN Logo" />
+            </div>
+            <!-- /Picture -->
+            <!-- Testimonial author information -->
+            <div class="testimonial-author-info">
+              <p class="testimonial-author">
+                <a
+                  href="https://www.cnn.com/2022/10/12/business/starbucks-rewards-delta-skymiles-partnership/index.html"
+                  >CNN
+                </a>
+              </p>
+              <p class="testimonial-firm">
+                Delta X Starbucks Skymiles Program Website
+              </p>
+            </div>
+          </div>
+          <!-- /Testimonial author information -->
         </div>
         <!-- End of Testimonial 1 -->
-        
+
         <!-- Testimonial 2 -->
         <div class="testimonial-item">
-            <!-- Testimonial Content -->
-            <div class="testimonial-content">
-                <div class="testimonial-text">
-                    
-                    <img src="images/press/delta-biz.png" alt="Delta Business">
-                </div>
-            </div>            
-            <!-- /Testimonial Content -->  
-            <!-- Testimonial Author -->
-            <div class="testimonial-credits">
-                <!-- Picture -->
-                <div class="testimonial-picture">
-                    <img src="images/press/delta-logo.png" alt="Delta Logo">
-                </div>              
-                <!-- /Picture -->
-                <!-- Testimonial author information -->
-                <div class="testimonial-author-info">
-                    <p class="testimonial-author">
-                      <a href="https://news.delta.com/delta-business-brings-robust-set-business-travel-tools-products-and-services-under-one-name">
-                          Delta News
-                      </a>  
-                    </p>
-                    <p class="testimonial-firm">Delta Business</p>
-                </div>
+          <!-- Testimonial Content -->
+          <div class="testimonial-content">
+            <div class="testimonial-text">
+              <img src="/images/press/delta-biz.png" alt="Delta Business" />
             </div>
-            <!-- /Testimonial author information -->               
+          </div>
+          <!-- /Testimonial Content -->
+          <!-- Testimonial Author -->
+          <div class="testimonial-credits">
+            <!-- Picture -->
+            <div class="testimonial-picture">
+              <img src="/images/press/delta-logo.png" alt="Delta Logo" />
+            </div>
+            <!-- /Picture -->
+            <!-- Testimonial author information -->
+            <div class="testimonial-author-info">
+              <p class="testimonial-author">
+                <a
+                  href="https://news.delta.com/delta-business-brings-robust-set-business-travel-tools-products-and-services-under-one-name"
+                >
+                  Delta News
+                </a>
+              </p>
+              <p class="testimonial-firm">Delta Business</p>
+            </div>
+          </div>
+          <!-- /Testimonial author information -->
         </div>
         <!-- End of Testimonial 2 -->
-        
+
         <!-- Testimonial 3 -->
         <div class="testimonial-item">
-            <!-- Testimonial Content -->
-            <div class="testimonial-content">
-                <div class="testimonial-text">
-                    <a href="https://www.forbes.com/sites/geoffwhitmore/2022/10/12/earn-miles-on-your-coffee-run-with-this-starbucks-and-delta-partnership/?sh=1c9c1f0d5a75">
-
-                        <img src="images/press/forbes-press.png" alt="Forbes Delta Starbucks">
-                    </a>
-                </div>
-              </div>            
-              <!-- /Testimonial Content -->  
-              <!-- Testimonial Author -->
-              <div class="testimonial-credits">
-                <!-- Picture -->
-                <div class="testimonial-picture">
-                  <img src="images/press/forbes-logo.jpg" alt="">
-                </div>              
-                <!-- /Picture -->
-                <!-- Testimonial author information -->
-                <div class="testimonial-author-info">
-                  <p class="testimonial-author">
-                <a href="https://www.forbes.com/sites/geoffwhitmore/2022/10/12/earn-miles-on-your-coffee-run-with-this-starbucks-and-delta-partnership/?sh=1c9c1f0d5a75">
-                    Forbes
-                </a>    
-                </p>
-                  <p class="testimonial-firm">
-                    Delta X Starbucks Skymiles Program Website
-                  </p>
-                </div>
-              </div>
-              <!-- /Testimonial author information -->               
+          <!-- Testimonial Content -->
+          <div class="testimonial-content">
+            <div class="testimonial-text">
+              <a
+                href="https://www.forbes.com/sites/geoffwhitmore/2022/10/12/earn-miles-on-your-coffee-run-with-this-starbucks-and-delta-partnership/?sh=1c9c1f0d5a75"
+              >
+                <img
+                  src="/images/press/forbes-press.png"
+                  alt="Forbes Delta Starbucks"
+                />
+              </a>
             </div>
-            <!-- End of Testimonial 3 -->
           </div>
+          <!-- /Testimonial Content -->
+          <!-- Testimonial Author -->
+          <div class="testimonial-credits">
+            <!-- Picture -->
+            <div class="testimonial-picture">
+              <img src="/images/press/forbes-logo.jpg" alt="" />
+            </div>
+            <!-- /Picture -->
+            <!-- Testimonial author information -->
+            <div class="testimonial-author-info">
+              <p class="testimonial-author">
+                <a
+                  href="https://www.forbes.com/sites/geoffwhitmore/2022/10/12/earn-miles-on-your-coffee-run-with-this-starbucks-and-delta-partnership/?sh=1c9c1f0d5a75"
+                >
+                  Forbes
+                </a>
+              </p>
+              <p class="testimonial-firm">
+                Delta X Starbucks Skymiles Program Website
+              </p>
+            </div>
+          </div>
+          <!-- /Testimonial author information -->
         </div>
+        <!-- End of Testimonial 3 -->
       </div>
+    </div>
+  </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
