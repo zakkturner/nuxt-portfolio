657be35f4cbca1b7182e3453bde2124095ab98f5
Zachary Turner <zakkturner@yahoo.com>
2023-07-04 15:07:37 -0400

    added pictures to site

--- components/subpages/AboutSubpage.vue
+++ components/subpages/AboutSubpage.vue
@@ -1,59 +1,109 @@
 <template>
-    <section class="pt-page pt-page-2" data-id="about_me">
-        <div class="border-block-top-110"></div>
-        <div class="section-inner">
-          <div class="section-title-block">
-            <div class="section-title-wrapper clearfix">
-              <h2 class="section-title">About Me</h2>
-              <h5 class="section-description">Creative Frontend Developer</h5>
-            </div>
+  <section class="pt-page pt-page-2" data-id="about_me">
+    <div class="border-block-top-110"></div>
+    <div class="section-inner">
+      <div class="section-title-block">
+        <div class="section-title-wrapper clearfix">
+          <h2 class="section-title">About Me</h2>
+          <h5 class="section-description">Creative Frontend Developer</h5>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-6 col-md-6 col-lg-4 subpage-block">
+          <div class="my-photo-block tilt-effect">
+            <img src="images/upwork.jpg" alt="" />
           </div>
+        </div>
 
-          <div class="row">
-            <div class="col-sm-6 col-md-6 col-lg-4 subpage-block">
-              <div class="my-photo-block tilt-effect">
-                <img src="images/main_photo.jpg" alt="">
-              </div>
-            </div>
+        <div class="col-sm-6 col-md-6 col-lg-4">
+          <h3>Experienced Front End Developer</h3>
+          <p>
+            I am a Javascript developer with experience working on high
+            visibility applications with companies, such as Delta Airlines and
+            Sean “Puffy'' Combs' Combs Enterprises. I focus on front end
+            frameworks such as React and Angular.
+          </p>
+          <p>
+            I have a passion for Creative Development (GSAP and Three.js), Web
+            Performance, Ecommerce Development, Web Accessibility, and headless
+            JAM stack (Javascript, API’s, Markup) Applications. To grow my
+            skills I focus on building a JAMstack application for a developer
+            community called Interactive Nerd, practice game dev with C# and
+            Unity as a hobby and occasionally freelance to help small businesses
+            in my community.
+          </p>
+        </div>
 
-            <div class="col-sm-6 col-md-6 col-lg-4">
-              <h3>Experienced Front End Developer</h3>
-              <p>I am a Javascript developer with experience working on high visibility applications with companies, such as Delta Airlines and Sean “Puffy'' Combs' Combs Enterprises. I focus on front end frameworks such as React and Angular.</p>
-              <p>I have a passion for Creative Development (GSAP and Three.js), Web Performance, Ecommerce Development, Web Accessibility, and headless JAM stack (Javascript, API’s, Markup) Applications. To grow my skills I focus on building a JAMstack application for a developer community called Interactive Nerd, practice game dev with C# and Unity as a hobby and occasionally freelance to help small businesses in my community.</p>
-            </div>
+        <div class="col-sm-6 col-md-6 col-lg-4 subpage-block">
+          <ul class="info-list">
+            <!-- <li><span class="title">Age</span><span class="value">29</span></li> -->
+            <!-- <li><span class="title">Residence</span><span class="value">Australia</span></li> -->
+            <li>
+              <span class="title">Address</span
+              ><span class="value">Atlanta, GA</span>
+            </li>
+            <li>
+              <span class="title">E-Mail</span
+              ><span class="value"
+                ><a href="mailto:email@example.com"
+                  >zakkturner1993@gmail.com</a
+                ></span
+              >
+            </li>
+            <li>
+              <span class="title">Phone</span
+              ><span class="value">317-728-0446</span>
+            </li>
+            <li>
+              <span class="title">Freelance</span
+              ><span class="value available">Available</span>
+            </li>
+          </ul>
 
-            <div class="col-sm-6 col-md-6 col-lg-4 subpage-block">
-              <ul class="info-list">
-                <!-- <li><span class="title">Age</span><span class="value">29</span></li> -->
-                <!-- <li><span class="title">Residence</span><span class="value">Australia</span></li> -->
-                <li><span class="title">Address</span><span class="value">Atlanta, GA</span></li>
-                <li><span class="title">E-Mail</span><span class="value"><a href="mailto:email@example.com">zakkturner1993@gmail.com</a></span></li>
-                <li><span class="title">Phone</span><span class="value">317-728-0446</span></li>
-                <li><span class="title">Freelance</span><span class="value available">Available</span></li>
-              </ul>
+          <ul class="social-links">
+            <li>
+              <a class="tip social-button" href="#" title="Twitter"
+                ><i class="fa fa-twitter"></i
+              ></a>
+            </li>
+            <!-- Full list of social icons: http://fontawesome.io/icons/#brand -->
+            <li>
+              <a class="tip social-button" href="#" title="Facebook"
+                ><i class="fa fa-facebook"></i
+              ></a>
+            </li>
+            <li>
+              <a class="tip social-button" href="#" title="Google Plus"
+                ><i class="fa fa-google-plus"></i
+              ></a>
+            </li>
+            <li>
+              <a class="tip social-button" href="#" title="Youtube"
+                ><i class="fa fa-youtube"></i
+              ></a>
+            </li>
+            <li>
+              <a class="tip social-button" href="#" title="Instagram"
+                ><i class="fa fa-instagram"></i
+              ></a>
+            </li>
+            <!--<li><a class="tip social-button" href="#" title="last.fm"><i class="fa fa-lastfm"></i></a></li>-->
+            <!--<li><a class="tip social-button" href="#" title="Dribbble"><i class="fa fa-dribbble"></i></a></li>-->
+          </ul>
+        </div>
+      </div>
 
-              <ul class="social-links">
-                <li><a class="tip social-button" href="#" title="Twitter"><i class="fa fa-twitter"></i></a></li> <!-- Full list of social icons: http://fontawesome.io/icons/#brand -->
-                <li><a class="tip social-button" href="#" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-                <li><a class="tip social-button" href="#" title="Google Plus"><i class="fa fa-google-plus"></i></a></li>
-                <li><a class="tip social-button" href="#" title="Youtube"><i class="fa fa-youtube"></i></a></li>
-                <li><a class="tip social-button" href="#" title="Instagram"><i class="fa fa-instagram"></i></a></li>
-                <!--<li><a class="tip social-button" href="#" title="last.fm"><i class="fa fa-lastfm"></i></a></li>-->
-                <!--<li><a class="tip social-button" href="#" title="Dribbble"><i class="fa fa-dribbble"></i></a></li>-->
-              </ul>
-            </div>
-          </div>
+      <!-- Services block -->
+      <div class="block-title">
+        <h3>Services</h3>
+      </div>
+      <Services />
 
-          <!-- Services block -->
-          <div class="block-title">
-            <h3>Services</h3>
-          </div>
-          <Services />
-          
-          <!-- End of Services block -->
+      <!-- End of Services block -->
 
-          <!-- Clients block -->
-          <!-- <div class="block-title">
+      <!-- Clients block -->
+      <!-- <div class="block-title">
             <h3>Clients</h3>
           </div>
 
@@ -94,21 +144,19 @@
               </div>
             </div>
           </div> -->
-          <!-- End of Clients block -->
+      <!-- End of Clients block -->
 
-        <!-- Press Block -->
-        <Press />
-          <!-- End of Press -->
-          <!-- Fun facts block -->
-       
-          <!-- End of Fun fucts block -->
-        </div>
-      </section>
+      <!-- Press Block -->
+      <Press />
+      <!-- End of Press -->
+      <!-- Fun facts block -->
+
+      <!-- End of Fun fucts block -->
+    </div>
+  </section>
 </template>
 <script setup lang="ts">
-import Services from '../about/Services.vue';
-import Press from '../about/Press.vue';
+import Services from "../about/Services.vue";
+import Press from "../about/Press.vue";
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
--- components/subpages/ResumeSubpage.vue
+++ components/subpages/ResumeSubpage.vue
@@ -1,95 +1,104 @@
 <template>
-    <section class="pt-page pt-page-3" data-id="resume">
-        <div class="border-block-top-110"></div>
-        <div class="section-inner">
-          <div class="section-title-block">
-            <div class="section-title-wrapper">
-              <h2 class="section-title">Resume</h2>
-              <h5 class="section-description">4 Years of Experience</h5>
-            </div>
-          </div>
+  <section class="pt-page pt-page-3" data-id="resume">
+    <div class="border-block-top-110"></div>
+    <div class="section-inner">
+      <div class="section-title-block">
+        <div class="section-title-wrapper">
+          <h2 class="section-title">Resume</h2>
+          <h5 class="section-description">4 Years of Experience</h5>
+        </div>
+      </div>
 
-          <div class="row">
-            
-            <div class="col-sm-6 col-md-4 subpage-block">
-              <div class="block-title">
-                <h3>Experience</h3>
-              </div>
-              <div class="timeline">
-                <!-- Single event -->
-                <div class="timeline-event te-primary">
-                  <h5 class="event-date">Nov 2022 - Apr 2023</h5>
-                  <h4 class="event-name">Frontend-developer</h4>
-                  <span class="event-description">Comb's Enterprises</span>
-                  <p>Frontend development with React and Typescript to create a marketplace for minority businesses.</p>
-                </div>
-                <!-- Single event -->
-                <div class="timeline-event te-primary">
-                  <h5 class="event-date">Nov 2021 - Nov 2022</h5>
-                  <h4 class="event-name">Frontend Developer</h4>
-                  <span class="event-description">Delta Airlines</span>
-                  <p>UI Development creating converting figma designs into high visibility sites including: https://www.business.delta.com and https://www.deltastarbucks.com/content/starbucks/en/overview.html</p>
-                </div>
-                <!-- Single event -->
-                <div class="timeline-event te-primary">
-                  <h5 class="event-date">March 2021 - Nov 2021</h5>
-                  <h4 class="event-name">Frontend Developer</h4>
-                  <span class="event-description">Incident IQ</span>
-                  <p>Marketing development such as creating HTML Emails, A/B landing pages with Unbounce, and creating and managing Wordpress themes</p>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-6 col-md-4 subpage-block">
-              <div class="block-title">
-                <h3>Experience cont.</h3>
-              </div>
-              <div class="timeline">
-                <!-- Single event -->
-                <div class="timeline-event te-primary">
-                  <h5 class="event-date">Sept 2019 - March 2021</h5>
-                  <h4 class="event-name">Shopify developer</h4>
-                  <span class="event-description">j5create</span>
-                  <p>Shopify theme development and Nuxt/Vue development for a microsite.</p>
-                </div>
-                <!-- Single event -->
-                <div class="timeline-event te-primary">
-                  <h5 class="event-date">March 2019 - Nov 2021</h5>
-                  <h4 class="event-name">Frontend Engineer</h4>
-                  <span class="event-description">Interactive Nerd</span>
-                  <p>Mauris magna sapien, pharetra consectetur fringilla vitae, interdum sed tortor.</p>
-                </div>
-              
-              </div>
-            </div>
-            <div class="col-sm-6 col-md-4 subpage-block">
-              <div class="block-title">
-                <h3>Education</h3>
-              </div>
-              <div class="timeline">
-                <!-- Single event -->
-                <div class="timeline-event te-primary">
-                  <h5 class="event-date">2015</h5>
-                  <h4 class="event-name">Telecommunications</h4>
-                  <span class="event-description">Ball State University</span>
-                  <!-- <p>Mauris magna sapien, pharetra consectetur fringilla vitae, interdum sed tortor.</p> -->
-                </div>
-               
-              </div>
-            </div>
-
+      <div class="row">
+        <div class="col-sm-6 col-md-4 subpage-block">
+          <div class="block-title">
+            <h3>Experience</h3>
           </div>
-          <div class="row">
-            <div class="col-sm-12 col-md-12">
-              <div class="download-cv-block">
-                <a class="button" target="_blank" href="#">Download CV</a>
-              </div>
+          <div class="timeline">
+            <!-- Single event -->
+            <div class="timeline-event te-primary">
+              <h5 class="event-date">Nov 2022 - Apr 2023</h5>
+              <h4 class="event-name">Frontend-developer</h4>
+              <span class="event-description">Comb's Enterprises</span>
+              <p>
+                Frontend development with React and Typescript to create a
+                marketplace for minority businesses.
+              </p>
+            </div>
+            <!-- Single event -->
+            <div class="timeline-event te-primary">
+              <h5 class="event-date">Nov 2021 - Nov 2022</h5>
+              <h4 class="event-name">Frontend Developer</h4>
+              <span class="event-description">Delta Airlines</span>
+              <p>
+                UI Development creating converting figma designs into high
+                visibility sites including: https://www.business.delta.com and
+                https://www.deltastarbucks.com/content/starbucks/en/overview.html
+              </p>
+            </div>
+            <!-- Single event -->
+            <div class="timeline-event te-primary">
+              <h5 class="event-date">March 2021 - Nov 2021</h5>
+              <h4 class="event-name">Frontend Developer</h4>
+              <span class="event-description">Incident IQ</span>
+              <p>
+                Marketing development such as creating HTML Emails, A/B landing
+                pages with Unbounce, and creating and managing Wordpress themes
+              </p>
             </div>
           </div>
         </div>
-      </section>
+        <div class="col-sm-6 col-md-4 subpage-block">
+          <div class="block-title">
+            <h3>Experience cont.</h3>
+          </div>
+          <div class="timeline">
+            <!-- Single event -->
+            <div class="timeline-event te-primary">
+              <h5 class="event-date">Sept 2019 - March 2021</h5>
+              <h4 class="event-name">Shopify developer</h4>
+              <span class="event-description">j5create</span>
+              <p>
+                Shopify theme development and Nuxt/Vue development for a
+                microsite.
+              </p>
+            </div>
+            <!-- Single event -->
+            <div class="timeline-event te-primary">
+              <h5 class="event-date">Jan 2019 - Present</h5>
+              <h4 class="event-name">Web Developer</h4>
+              <span class="event-description">Freelance</span>
+              <p>
+                Working on various projects with technologies including React,
+                Vue, Laravel, Wordpress, and Shopify
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 subpage-block">
+          <div class="block-title">
+            <h3>Education</h3>
+          </div>
+          <div class="timeline">
+            <!-- Single event -->
+            <div class="timeline-event te-primary">
+              <h5 class="event-date">2015</h5>
+              <h4 class="event-name">Telecommunications</h4>
+              <span class="event-description">Ball State University</span>
+              <!-- <p>Mauris magna sapien, pharetra consectetur fringilla vitae, interdum sed tortor.</p> -->
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-12 col-md-12">
+          <div class="download-cv-block">
+            <a class="button" target="_blank" href="#">Download CV</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
-<script lang="ts" setup>
-</script>
-<style lang="">
-    
-</style>
+<script lang="ts" setup></script>
+<style lang=""></style>
